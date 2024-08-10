b1629ec8c1c832bb5b9dd2e4757c1f196f822163
ashish <aashidare123@gmail.com>
2024-08-11 00:30:19 +0530

    second commit

--- src/com/org/controller/EmployeeController.java
+++ src/com/org/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.org.controller;
 
+<<<<<<< HEAD
+=======
+import java.util.List;
+>>>>>>> f46a9d1 (second commit)
 import java.util.Scanner;
 
 import com.org.dao.EmployeeDao;
 import com.org.dto.Employee;
 
 public class EmployeeController {
+<<<<<<< HEAD
 	
 	public static void main(String[] args) {
 		
@@ -31,6 +36,134 @@ public class EmployeeController {
 		
 		Employee emp = dao.fetchEmployeeById(1);
 		System.out.println(emp);
+=======
+
+	public static void main(String[] args) {
+
+	EmployeeController emp= new EmployeeController();
+	emp.employeeController();
+
+	}
+	
+	public void employeeController() {
+		
+
+		System.out.println("**********HOMEPAGE*********");
+
+		System.out.println("1. Insert Employee");
+		System.out.println("2. Fetch Employee");
+		System.out.println("3. Fetch All Employees");
+		System.out.println("4. Update Employee");
+		System.out.println("5. Delete Employee");
+		System.out.println("6. Exit");
+
+		System.out.println("Choose your choice");
+		Scanner os = new Scanner(System.in);
+
+		int choice = os.nextInt();
+		EmployeeDao dao = new EmployeeDao();
+
+		switch (choice) {
+		case 1: {
+			
+			System.out.println("Enter Employee Name : ");
+			String name= os.next();
+			System.out.println("Enter Employee Age : ");
+			int age = os.nextInt();
+			System.out.println("Enter Employee Mobile : ");
+			long mobile= os.nextLong();
+			System.out.println("Enter Employee Email : ");
+			String email = os.next();
+			System.out.println("Enter Employee Password : ");
+			os.nextLine();
+			String password=os.next();
+			Employee emp = new Employee();
+
+			emp.setName(name);
+			emp.setAge(age);
+			emp.setMobile(mobile);
+			emp.setEmail(email);
+			emp.setPassword(password);
+		
+
+			dao.InsertEmployee(emp);
+			break;
+			
+
+		}
+		case 2: {
+			System.out.println("Enter the Employee Id to Update: ");
+			int id = os.nextInt();
+			Employee emp = dao.fetchEmployeeById(id);
+			System.out.println(emp);
+			break;
+
+		}
+		case 3: {
+			
+			
+
+			List<Employee> listOfEmployees = dao.fetchall();
+			System.out.println(listOfEmployees);
+			break;
+
+		}
+		case 4: {
+			
+			System.out.println("Enter the Employee Id : ");
+		
+			int id = os.nextInt();
+			System.out.println("Enter Employee Name : ");
+			String name= os.next();
+			System.out.println("Enter Employee Age : ");
+			int age = os.nextInt();
+			System.out.println("Enter Employee Mobile : ");
+			long mobile= os.nextLong();
+			System.out.println("Enter Employee Email : ");
+			String email = os.next();
+			System.out.println("Enter Employee Password : ");
+			os.nextLine();
+			String password=os.next();
+			Employee emp = new Employee();
+			emp.setName(name);
+			emp.setAge(age);
+			emp.setMobile(mobile);
+			emp.setEmail(email);
+			emp.setPassword(password);
+			dao.updateEmployeeById(id, emp);
+			break;
+
+		}
+		case 5: {
+			
+			System.out.println("Enter Employee Id to Delete: ");
+			int id = os.nextInt();
+			dao.deleteEmployeeById(id);
+			break;
+			
+
+
+		}
+		case 6: {
+			
+			return;
+
+		}
+		default:
+			System.out.println("Invalid Option Chosen");
+			break;
+			
+		}
+		
+		employeeController();
+		
+		
+		
+		
+		
+		
+		
+>>>>>>> f46a9d1 (second commit)
 	}
 
 }
--- src/com/org/dao/EmployeeDao.java
+++ src/com/org/dao/EmployeeDao.java
@@ -5,6 +5,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+<<<<<<< HEAD
+=======
+import java.util.ArrayList;
+import java.util.List;
+>>>>>>> f46a9d1 (second commit)
 
 import com.org.dto.Employee;
 
@@ -31,6 +36,10 @@ public class EmployeeDao {
 			    
 			    stmt.close();
 			    con.close();
+<<<<<<< HEAD
+=======
+				System.out.println("Data Inserted");
+>>>>>>> f46a9d1 (second commit)
 			    
 				
 				
@@ -40,7 +49,11 @@ public class EmployeeDao {
 			}
 	}
 			
+<<<<<<< HEAD
 		public Employee fetchEmployeeById(int id) {
+=======
+		public Employee fetchEmployeeById (int id) {
+>>>>>>> f46a9d1 (second commit)
 			Employee emp= new Employee();
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
@@ -66,8 +79,107 @@ public class EmployeeDao {
 			    stmt.close();
 			    con.close();
 			    
+<<<<<<< HEAD
 			    return emp;
+=======
+			    
+>>>>>>> f46a9d1 (second commit)
 			   
+				
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+<<<<<<< HEAD
+=======
+			return emp;
+			
+			
+		}
+		
+		
+		public List<Employee> fetchall(){
+			
+			List<Employee> list= new ArrayList<Employee>();
+		
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/first_jdbc","root","ashish");
+				
+				
+				PreparedStatement stmt = con.prepareStatement("select * from employee ");
+				
+				
+				
+			    ResultSet rst = stmt.executeQuery();
+			    
+			    while(rst.next()) {
+			    	Employee emp1= new Employee();
+			    emp1.setId(rst.getInt("id")); 
+			    emp1.setName(rst.getString("name"));
+			    emp1.setAge(rst.getInt("age"));
+			    emp1.setMobile(rst.getLong("mobile"));
+			    emp1.setEmail(rst.getString("email"));
+			    emp1.setPassword(rst.getString("password"));
+			    list.add(emp1);
+			    
+			    	
+			    }
+			    
+			   
+			    rst.close();
+			    stmt.close();
+			    con.close();
+			    
+			
+			   
+				
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return list;
+			
+			
+			
+		
+			
+			
+			
+			
+			
+		
+			
+			
+			
+			
+			
+		}
+		
+		public void updateEmployeeById(int id,Employee emp) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/first_jdbc","root","ashish");
+				
+				
+				PreparedStatement stmt = con.prepareStatement("update employee set name=?,age=?,mobile=?,email=?,password=? where id=?");
+				
+				stmt.setString(1,emp.getName());
+				stmt.setInt(2,emp.getAge());
+				stmt.setLong(3,emp.getMobile());
+				stmt.setString(4,emp.getEmail());
+				stmt.setString(5,emp.getPassword());
+				stmt.setInt(6, id);
+				
+				
+			    stmt.executeUpdate();
+			    
+			    stmt.close();
+			    con.close();
+			    System.out.println("Data Updated");
+			    
 				
 				
 			} catch (ClassNotFoundException | SQLException e) {
@@ -76,9 +188,41 @@ public class EmployeeDao {
 			}
 			
 			
+			
+			
+>>>>>>> f46a9d1 (second commit)
+			
+			
 		}
 		
+<<<<<<< HEAD
 		
+=======
+		public void deleteEmployeeById(int id) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/first_jdbc","root","ashish");
+				
+				
+				PreparedStatement stmt = con.prepareStatement("delete from employee where id=?");
+				
+				stmt.setInt(1,id);
+				
+			    stmt.executeUpdate();
+			    
+			    stmt.close();
+			    con.close();
+			    System.out.println("Data Deleted");
+			    
+				
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+>>>>>>> f46a9d1 (second commit)
 		
 		
 			
